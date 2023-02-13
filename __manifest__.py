95be7157e817c9d9fe0c5628a5d094cc0db7e3f1
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2023-02-13 06:56:12 -0800

    ok

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Mate',
+    'summary': 'Hospital management system (Mate)',
+    'description': """ Hospital management system """,
+    'depends': ['sales_team', 'payment', 'portal', 'utm'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+}
